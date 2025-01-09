08d1c644d24ea1b15983830354b019cadb677b37
Akash AR <akuaka750@gmail.com>
2025-01-09 20:48:24 +0530

    first commit

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses>{
+
+final List<Expense> _registeredExpenses = [
+  Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
+  Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure),
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+          onPressed: (){}, 
+          icon: const Icon(Icons.add))
+        ],
+      ),
+      body: Column(
+        children: [
+          Text('The Chart'),
+         Expanded(
+          child:ExpensesList(expenses:_registeredExpenses
+          ),
+          ) 
+        ],
+      ),
+    );
+  }
+  
+}
---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:expense_tracker_app/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget{
+  const ExpensesItem(this.expenses , {super.key});
+
+  final Expense expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding:  EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical:16
+        ),
+        child: Column(
+          children:[
+            Text(expenses.title),
+            const SizedBox(height:4),
+            Row(
+              children: [
+                Text('\$${expenses.amount.toStringAsFixed(2)}'),
+                Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expenses.category]),
+                    const SizedBox(width: 8),
+                    Text(expenses.formattedDate)
+                  ],
+                ),
+              ]
+            )
+          ]
+        ),
+      ),
+    );
+  } 
+  
+}
